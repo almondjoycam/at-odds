9f375d1e7e3b09d31e91a4bf01aebaf0f370332e
Camber Boles <camberb51@gmail.com>
2026-06-13 15:14:30 -0400

    Crop cards and add to material resource

--- poker/card_mats.gd
+++ poker/card_mats.gd
@@ -1,10 +1,10 @@
 class_name CardMaterials extends Resource
 
 @export var deck_back: Texture2D
-@export var heart_mats: Dictionary[int, Texture2D]
-@export var spade_mats: Dictionary[int, Texture2D]
-@export var diamond_mats: Dictionary[int, Texture2D]
-@export var club_mats: Dictionary[int, Texture2D]
+@export var heart_mats: Array[Texture2D]
+@export var spade_mats: Array[Texture2D]
+@export var diamond_mats: Array[Texture2D]
+@export var club_mats: Array[Texture2D]
 
 func texture_from_card_data(suit: Card.Suit, rank: int) -> Texture2D:
 	match suit:
